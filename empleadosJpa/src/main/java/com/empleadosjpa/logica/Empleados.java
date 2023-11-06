0740fc9bb5b778740bd2219921e42a6c442328b6
Oscar2912 <oscar.cuencamartin@gmail.com>
2023-11-06 12:54:20 +0100

    commit sistema empleados funcional, probado y comentado v 1.0

---
+++ empleadosJpa/src/main/java/com/empleadosjpa/logica/Empleados.java
@@ -0,0 +1,92 @@
+package com.empleadosjpa.logica;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="empleados")
+public class Empleados implements Serializable {
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String cargo;
+    private double salario;
+    private String fechaInicio;
+    
+    //Contructores
+
+    public Empleados() {
+    }
+
+    public Empleados(int id, String nombre, String apellido, String cargo, double salario, String fechaInicio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.fechaInicio = fechaInicio;
+    }
+    
+    //Métodos
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleados{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", cargo=" + cargo + ", salario=" + salario + ", fechaInicio=" + fechaInicio + '}';
+    }
+    
+}
---
+++ empleadosJpa/target/generated-sources/annotations/com/empleadosjpa/logica/Empleados_.java
@@ -0,0 +1,18 @@
+package com.empleadosjpa.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-06T12:21:10", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Empleados.class)
+public class Empleados_ { 
+
+    public static volatile SingularAttribute<Empleados, String> fechaInicio;
+    public static volatile SingularAttribute<Empleados, String> apellido;
+    public static volatile SingularAttribute<Empleados, Double> salario;
+    public static volatile SingularAttribute<Empleados, Integer> id;
+    public static volatile SingularAttribute<Empleados, String> cargo;
+    public static volatile SingularAttribute<Empleados, String> nombre;
+
+}
