a772ce9ccae9d5354212ca6711bf9f386a878460
Oscar2912 <oscar.cuencamartin@gmail.com>
2023-11-08 14:02:11 +0100

    se añade proyecto y readme tras fallo en github. proyecto documentado, probado y actualizado con modulos try catch

---
+++ empleadosJpa/src/main/java/com/empleadosjpa/persistencia/EmpleadosJpaController.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.empleadosjpa.persistencia;
+
+import com.empleadosjpa.logica.Empleados;
+import com.empleadosjpa.persistencia.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author Oscar
+ */
+public class EmpleadosJpaController implements Serializable {
+
+    public EmpleadosJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EmpleadosJpaController() {
+        emf = Persistence.createEntityManagerFactory("empleadosPU");
+    }
+    
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Empleados empleados) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(empleados);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Empleados empleados) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            empleados = em.merge(empleados);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                int id = empleados.getId();
+                if (findEmpleados(id) == null) {
+                    throw new NonexistentEntityException("The empleados with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(int id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Empleados empleados;
+            try {
+                empleados = em.getReference(Empleados.class, id);
+                empleados.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("\n**** ERROR ****\nEl empleado con id " + id + " no existe.");
+            }
+            em.remove(empleados);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Empleados> findEmpleadosEntities() {
+        return findEmpleadosEntities(true, -1, -1);
+    }
+
+    public List<Empleados> findEmpleadosEntities(int maxResults, int firstResult) {
+        return findEmpleadosEntities(false, maxResults, firstResult);
+    }
+
+    private List<Empleados> findEmpleadosEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Empleados.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Empleados findEmpleados(int id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Empleados.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getEmpleadosCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Empleados> rt = cq.from(Empleados.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
